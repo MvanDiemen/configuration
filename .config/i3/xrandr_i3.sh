44a5bf3b53aaf87079bf9dc6aeec1ee07bf6fb1b
Michaël van Diemen <van.diemen.michael@gmail.com>
2021-06-11 08:56:56 +0200

    Add various tweaks
    - Inclusion of spotify controls on keyboard and polybar
    - Picom config changes to prevent crashes due to blurring
    - Add gitconfig for git LFS
    - Small tweaks to polybar modules

--- .config/i3/xrandr_i3.sh
+++ .config/i3/xrandr_i3.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-if xrandr | grep "^HDMI.* connected" -A 2 | grep "2560x1440"; then
-  NAME="$(xrandr | grep '^HDMI.* connected' | grep -E -o '^\S*')"
-  xrandr --output $NAME --mode "2560x1440" --auto --output eDP1 --off
-elif xrandr | grep "^DP1 connected" -A 2 | grep "2560x1440"; then
-  autorandr home-display
-elif xrandr | grep "^DP3 connected" -A 2 | grep "2560x1440"; then
-  autorandr home-display
-elif xrandr | grep "^DP3-8 connected" -A 2 | grep "2560x1440"; then
-  autorandr home-display
-elif xrandr | grep "^eDP.* connected" -A 2 | grep "3840x2400"; then
-  autorandr laptop
-elif xrandr | grep "^eDP.* connected" -A 2 | grep "2560x1440"; then
-  xrandr --output eDP1 --mode "2560x1440"--primary --auto
-else
-  xrandr --output eDP1 --mode 1920x1080
-fi
+# Get the first detected option and use that one
+autorandr $(autorandr --detected | head -n 1)
+
+# Old xrandr scripts
+# if xrandr | grep "^HDMI.* connected" -A 2 | grep "2560x1440"; then
+#   NAME="$(xrandr | grep '^HDMI.* connected' | grep -E -o '^\S*')"
+#   xrandr --output $NAME --mode "2560x1440" --auto --output eDP1 --off
+# elif xrandr | grep "^DP1 connected" -A 2 | grep "2560x1440"; then
+#   autorandr home-display
+# elif xrandr | grep "^DP3 connected" -A 2 | grep "2560x1440"; then
+#   autorandr home-display
+# elif xrandr | grep "^DP3-8 connected" -A 2 | grep "2560x1440"; then
+#   autorandr home-display
+# elif xrandr | grep "^eDP.* connected" -A 2 | grep "3840x2400"; then
+#   autorandr laptop
+# elif xrandr | grep "^eDP.* connected" -A 2 | grep "2560x1440"; then
+#   xrandr --output eDP1 --mode "2560x1440"--primary --auto
+# else
+#   xrandr --output eDP1 --mode 1920x1080
+# fi
