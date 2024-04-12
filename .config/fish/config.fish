65fe04526e9515d5f42b533fb2665f2aa58ca500
Michaël van Diemen <michael.vandiemen@wearewuunder.com>
2024-04-12 10:04:08 +0200

    Set firefox as default browser and add more aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,15 +3,15 @@ export ERL_AFLAGS="-kernel shell_history enabled"
 set -x EDITOR '/usr/bin/nvim'
 set -x VISUAL '/usr/bin/nvim'
 
-set -Ux LC_ALL 'en_GB.UTF8'
-set -Ux LANG 'en_GB.UTF8'
+#set -Ux LC_ALL 'en_GB.UTF8'
+#set -Ux LANG 'en_GB.UTF8'
 
 set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --follow'
 set -U FZF_LEGACY_KEYBINDINGS 0
 
 set -x TERM 'xterm-256color'
 set -Ux EDITOR '/usr/bin/nvim'
-set -Ux BROWSER '/usr/bin/brave'
+set -Ux BROWSER '/usr/bin/firefox'
 set -Ux GIT_EDITOR 'nvim'
 set -x NVIM_TUI_ENABLE_TRUE_COLOR 1
 
@@ -43,15 +43,17 @@ set -g fish_prompt_pwd_dir_length 1
 set -g theme_display_ruby no
 set -g theme_project_dir_length 1
 
-source /opt/asdf-vm/asdf.fish
+# source /opt/asdf-vm/asdf.fish
 
 alias gc="git checkout"
-alias gpull="git pull"
-alias gpush="git push"
-alias gc-="git checkout -- ."
+alias gpull="git pull --no-tags"
+alias gpush="git push --no-tags"
+alias gc-="git checkout -"
+alias gc--="git checkout -- ."
 alias gs="git status"
+alias gf="git fetch -p --no-tags"
 alias iserver="iex -S mix phx.server"
-alias depsget="mix deps.get"
+alias mixdeps="mix deps.get"
 alias migrate="mix ecto.migrate"
 
 begin
