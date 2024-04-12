51215496858a70bd1cb881c13ee3be17c8bf129d
Michaël van Diemen <michael.vandiemen@wearewuunder.com>
2024-04-12 10:04:52 +0200

    Use spt for polybar and setup for all displays

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,8 +9,17 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 DISPLAY1="$(xrandr -q | grep '^HDMI.* connected' | grep '.*x.* ' | cut -d ' ' -f1)"
   [[ ! -z "$DISPLAY1" ]] && MONITOR="${DISPLAY1}" polybar top &
 
+DISPLAY1="$(xrandr -q | grep '^HDMI-.* connected' | grep '.*x.* ' | cut -d ' ' -f1)"
+  [[ ! -z "$DISPLAY1" ]] && MONITOR="${DISPLAY1}" polybar top &
+
 DISPLAY2="$(xrandr -q | grep '^DP.* connected' | grep '.*x.* ' | cut -d ' ' -f1)"
   [[ ! -z "$DISPLAY2" ]] && MONITOR="${DISPLAY2}" polybar top &
+
+DISPLAY2="$(xrandr -q | grep '^DP-.* connected' | grep '.*x.* ' | cut -d ' ' -f1)"
+  [[ ! -z "$DISPLAY2" ]] && MONITOR="${DISPLAY2}" polybar top &
+
+DISPLAY3="$(xrandr -q | grep '^eDP-1 connected' | grep '.*x.* ' | cut -d ' ' -f1)"
+  [[ ! -z "$DISPLAY3" ]] && MONITOR="${DISPLAY3}" polybar top &
 
 DISPLAY6="$(xrandr -q | grep '^eDP1 connected' | grep '.*x.* ' | cut -d ' ' -f1)"
   [[ ! -z "$DISPLAY6" ]] && MONITOR="${DISPLAY6}" polybar top &
